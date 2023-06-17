96267980368dbfe99b1b51898f65fecc8524aeb3
Abinash Mainali <mainali.abinash@gmail.com>
2023-06-17 10:32:24 +1000

    Laravel server added

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -8,7 +8,7 @@ export default function Create(){
         <div className="container">
             <header className="jumbotron">
                 <h3>Create</h3>
-                <iframe src={"https://triviacontest.azurewebsites.net"} width="100%" height="1000px"></iframe>
+                <iframe src={"https://triviacontest.azurewebsites.net"} title={"Trivia"} width="100%" height="800px" ></iframe>
             </header>
         </div>
     )
