0555b00ee58228adb365e9d508eee5d7e1e29154
Abinash Mainali <mainali.abinash@gmail.com>
2023-06-17 10:10:26 +1000

    Installed axios to handle api

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,8 +1,14 @@
+
+
 export default function Create(){
+
+
+
     return(
         <div className="container">
             <header className="jumbotron">
                 <h3>Create</h3>
+                <iframe src={"https://triviacontest.azurewebsites.net"} width="100%" height="1000px"></iframe>
             </header>
         </div>
     )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,48 @@
-export default function Home(){
-    return(
-        <div className="container">
-            <header className="jumbotron">
-                <h3>Home</h3>
-            </header>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import http from "../helper/http";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  const fetchAllUsers = () => {
+    http
+      .get("/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="container">
+      <h2>User Listing</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={user.id}>
+                <td>{index}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>edit</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
